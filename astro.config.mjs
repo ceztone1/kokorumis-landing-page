8be64ae7638c4aebebf086b0acae4160863bf717
Guillermo Alvarez <guillermo.alvarez@ugto.mx>
2025-01-22 19:22:29 -0600

    Inclusion configuracion astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
