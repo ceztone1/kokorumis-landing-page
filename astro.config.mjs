be58a7f5a4078e16ee78bb1f145470add5873195
Guillermo Alvarez <guillermo.alvarez@ugto.mx>
2025-01-22 21:35:46 -0600

    Inclusion de configuracion para uso de vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-
+import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    vue(),
   ],
 });
