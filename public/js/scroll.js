2136c0bbb413076fd0218c15daaaeaeda0eaf349
Guillermo Alvarez <guillermo.alvarez@ugto.mx>
2025-02-11 13:33:58 -0600

    Correccion uso de vue para header

--- public/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-  const header = document.getElementById('main-header');
-  if (window.scrollY > 35) {
-    header?.classList.add('scrolled');
-  } else {
-    header?.classList.remove('scrolled');
-  }
-});
