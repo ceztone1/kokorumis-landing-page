5523494008a75d49addbf3cd1dc2e3fa2ccc1554
Guillermo Alvarez <guillermo.alvarez@ugto.mx>
2025-02-11 12:50:36 -0600

    Correccion de uso de astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-    vue(),
-  ],
+  integrations: [tailwind(), vue()],
 });
